43623fc1f55b9a67a2e30bf0c718c357448bd560
ilevd <ilevd@mail.ru>
2024-11-23 19:26:45 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.ilevd/cwp "0.1.1"
-  :description "Indentation based syntax for Clojure"
+  :description "Indentation-based syntax for Clojure"
   :url "https://github.com/ilevd/cwp"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
